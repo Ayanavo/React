4d0358bbc2438c01aeb29d21a7bef8f888f31e4d
Ayanavo <ayanavo2057@gmail.com>
2024-09-29 22:05:55 +0530

    sync file

--- src/shared/controls/file.tsx
+++ src/shared/controls/file.tsx
@@ -32,7 +32,7 @@ function file({ form, schema }: { form: FieldValue<any>; schema: FileSchema }) {
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                 <span className="font-semibold">Click to upload</span> or drag and drop
               </p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">Max Size {schema.validation.maxSize || "500Mb"}</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">Max Size: {schema.validation.maxSize || "500Mb"}</p>
             </div>
             <input ref={inputRef} id="dropzone-file" type="file" className="hidden" />
           </div>
