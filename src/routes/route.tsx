d8212f1c754ae1a1f81feee88219d009324aae18
Ayanavo <ayanavo2057@gmail.com>
2024-11-19 22:49:14 +0530

    sync login

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,21 +1,23 @@
+import ActivityComponent from "@/pages/activity/activity";
+import LoginComponent from "@/pages/auth/login/login";
+import RegistrationComponent from "@/pages/auth/registration/registration";
+import DashboardComponent from "@/pages/dashboard/dashboard";
+import ProfileComponent from "@/pages/profile/profile";
+import SettingsComponent from "@/pages/settings/settings";
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import DetailComponent from "../pages/layout/details/details";
 import { Layout } from "../pages/layout/layout";
 import TableComponent from "../pages/layout/table/table";
-import NoPageComponent from "../pages/nopage";
-import LoginComponent from "@/pages/auth/login/login";
-import DashboardComponent from "@/pages/dashboard/dashboard";
-import ActivityComponent from "@/pages/activity/activity";
-import ProfileComponent from "@/pages/profile/profile";
-import SettingsComponent from "@/pages/settings/settings";
-import UpdateComponent from "../pages/layout/update/update";
 import CreateComponent from "../pages/layout/update/create";
+import UpdateComponent from "../pages/layout/update/update";
+import NoPageComponent from "../pages/nopage";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginComponent />} />
+      <Route path="/registration" element={<RegistrationComponent />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="/dashboard" replace />} />
         <Route caseSensitive path="/dashboard" element={<DashboardComponent />} />
