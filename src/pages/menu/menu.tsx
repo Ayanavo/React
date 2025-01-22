1c031d2de13760e602bf2b9b0f6a7c04e13a28b0
Ayanavo <ayanavo2057@gmail.com>
2025-01-22 22:37:42 +0530

    sync sidebar color

--- src/pages/menu/menu.tsx
+++ src/pages/menu/menu.tsx
@@ -17,7 +17,7 @@ function menu({ NavList, isExpanded }: { NavList: Array<NavItem>; isExpanded: bo
         <SidebarMenu className="items-end">
           <SidebarMenuItem>
             <div className="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
-              <SidebarTrigger />
+              <SidebarTrigger className="text-primary hover:text-primary" />
             </div>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -32,7 +32,7 @@ function menu({ NavList, isExpanded }: { NavList: Array<NavItem>; isExpanded: bo
                   <TooltipTrigger asChild>
                     <SidebarMenuItem>
                       <SidebarMenuButton onClick={() => navigate(route)}>
-                        <IconsComponent customClass="h-6 w-6" icon={icon} />
+                        <IconsComponent customClass="h-6 w-6 text-primary" icon={icon} />
                         <span>{label}</span>
                       </SidebarMenuButton>
                     </SidebarMenuItem>
