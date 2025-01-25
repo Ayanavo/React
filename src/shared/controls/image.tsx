154cc4dce6431712bcf3a0a3ba03c0f364e04c71
Ayanavo <ayanavo2057@gmail.com>
2025-01-25 21:40:10 +0530

    sync multi file upload

--- src/shared/controls/image.tsx
+++ src/shared/controls/image.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Upload } from "lucide-react";
-import React, { ChangeEvent, useRef } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import { FieldValue } from "react-hook-form";
 type ImageSchema = {
   name: string;
@@ -13,12 +13,14 @@ type ImageSchema = {
 
 function image({ form, schema }: { form: FieldValue<any>; schema: ImageSchema }) {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [imagePreview, setImagePreview] = useState("");
 
   const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
+        setImagePreview(reader.result as string);
         form.setValue(schema.name, reader.result as string);
       };
       reader.readAsDataURL(file);
@@ -33,18 +35,26 @@ function image({ form, schema }: { form: FieldValue<any>; schema: ImageSchema })
       <div className="flex items-center gap-4">
         <input ref={inputRef} id="image-drop" type="file" accept="image/*" className="hidden" onChange={handleImageUpload} />
         <div className="relative w-16 h-16 border rounded-lg overflow-hidden">
-          {!form.getValues(schema.name) ?
+          {!imagePreview ?
             <div className="absolute inset-0 flex items-center justify-center bg-primary">
               <div className="text-4xl text-secondary transition duration-500 text-center">AL</div>
             </div>
-          : <img width={64} height={64} alt="Logo" src={form.getValues(schema.name)} className="profile-image" />}
+          : <img width={64} height={64} alt="Logo" src={imagePreview} className="profile-image" />}
         </div>
         <Button variant="outline" type="button" size="sm" onClick={() => inputRef.current?.click()}>
           <Upload className="mr-2 h-4 w-4" />
           Upload Image
         </Button>
-        {form.getValues(schema.name) && (
-          <Button variant="ghost" type="button" size="sm" className="text-muted-foreground" onClick={() => form.setValue("")}>
+        {imagePreview && (
+          <Button
+            variant="ghost"
+            type="button"
+            size="sm"
+            className="text-muted-foreground"
+            onClick={() => {
+              form.setValue("");
+              setImagePreview("");
+            }}>
             Remove
           </Button>
         )}
