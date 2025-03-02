d0a295fb1a94f36d9c4b3b8dc0bca52ee147af23
Ayanavo <ayanavo2057@gmail.com>
2025-03-02 22:37:57 +0530

    sync server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,37 @@
-import 'dotenv/config'; // Load environment variables
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import connectDB from './utils/db.js'; // Import the MongoDB connection utility
-import activityRoutes from './routes/activityRoutes.js'; // Import the new activity routes
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import i18n from "./utils/i18n.js";
+import bodyParser from "body-parser";
+import connectDB from "./utils/db.js";
+import activityRoutes from "./routes/activityRoutes.js";
+import serverlessHttp from "serverless-http";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 5000; // Convert PORT to a number
-const HOST = process.env.HOST || '0.0.0.0';
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = Number(process.env.PORT) || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 // Connect to MongoDB
 connectDB();
 
-// Use the /api/activities route
-app.use('/api/activities', activityRoutes);
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(i18n.init);
 
-// Default route
-app.get('/', (req, res) => {
-  res.send('Hello from Express with TypeScript and ES6!');
+app.use((req: Request, _res: Response, next: () => void) => {
+  const lang = req.query.lang as string;
+  if (lang) req.setLocale(lang); // Set the locale dynamicall
+  next();
 });
+
+// Use the /api/activities route
+// app.use("/api/activities", activityRoutes);
+app.use("/.netlify/functions/api/activities", activityRoutes);
 
 // Start the server
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
+
+serverlessHttp(app);
---
+++ server/src/utils/i18n.ts
@@ -0,0 +1,15 @@
+import i18n from "i18n";
+import path from "path";
+
+i18n.configure({
+  locales: ["en", "bn"], // Supported languages: English and Bengali
+  directory: path.join(__dirname, "../locales"), // Path to translation files
+  defaultLocale: "en", // Default language
+  autoReload: true, // Automatically reload translations when they change
+  syncFiles: true, // Automatically add missing keys to all files
+  objectNotation: true, // Allow nested JSON keys
+  cookie: "lang", // Use cookies to store the language preference
+});
+
+// Export i18n for use in other files
+export default i18n;
