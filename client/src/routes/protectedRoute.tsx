5fa853eb6fdc8aefb50e9a91a90d63e4553ca039
Ayanavo Lahiri <60645328+Ayanavo@users.noreply.github.com>
2026-05-31 07:19:58 +0000

    installed socketio

--- client/src/routes/protectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-function protectedRoute() {
-  const isLoggedIn = !!sessionStorage.getItem("auth_token");
-  return isLoggedIn ? <Outlet /> : <Navigate to={"/login"} />;
-}
-
-export default protectedRoute;
